63d952fa48a0f011020337958030a7468f9efaf0
kinopotato <kinoiskino@gmail.com>
2019-03-11 00:45:18 +0800

    added readMe file but used it as a change Log, so I guess changelog is coming next

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # iasdev-frontpage
 Unofficial front page for IasDev team containing all the information needed for new members to get started.
+
+v1: 
+-Everything is working but still needs a lot of work on navigation, transition and layout to be honest.
+-Break points are currently nonexistent. I worked with 13 inch 1366 x 720 screen, it looks passable there and slightly vomit inducing in everything else.
+-Haven't checked for typos yet.
+-I probably should make a changelog instead of putting this in the readMe.
+-Needs some bling, no JS yet.
+-For anyone reading this, the cat photo is a placeholder. . . but does anything else really deserve that spot
+-Meta data is barebones.
--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-// //switch front page from home to about
-// function switchFront () {
-//   document.querySelectorAll("#mainWindow").forEach(function(elem, index){
-//     elem.classList.toggle("display--hidden");
-//   })
-//   document.querySelector("#about").textContent === "home"? document.querySelector("#about").textContent = "about" : document.querySelector("#about").textContent = "home";
-// }
+// // //switch front page from home to about
+// // function switchFront () {
+// //   document.querySelectorAll("#mainWindow").forEach(function(elem, index){
+// //     elem.classList.toggle("display--hidden");
+// //   })
+// //   document.querySelector("#about").textContent === "home"? document.querySelector("#about").textContent = "about" : document.querySelector("#about").textContent = "home";
+// // }
 
-// document.querySelector("#about").addEventListener("click", switchFront);
+// // document.querySelector("#about").addEventListener("click", switchFront);
 
-// //switch between FAQs and links
-// function switchLinks () {
-//   document.querySelectorAll("#subWindow").forEach(function(elem, index){
-//     elem.classList.toggle("display--hidden");
-//   })
-//   document.querySelector("#about").textContent === "FAQs"? document.querySelector("#links").textContent = "FAQs" : document.querySelector("#about").textContent = "home";
-// }
+// // //switch between FAQs and links
+// // function switchLinks () {
+// //   document.querySelectorAll("#subWindow").forEach(function(elem, index){
+// //     elem.classList.toggle("display--hidden");
+// //   })
+// //   document.querySelector("#about").textContent === "FAQs"? document.querySelector("#links").textContent = "FAQs" : document.querySelector("#about").textContent = "home";
+// // }
 
-// document.querySelector("#faqs").addEventListener("click", switchLinks);
+// // document.querySelector("#faqs").addEventListener("click", switchLinks);
 
-//switch main display element via clicking nav buttons
+// //switch main display element via clicking nav buttons
 
-function switchFront (id) {
-  let sectionArr = document.querySelectorAll(".section");
-  let aboutBtn = document.querySelector("#aboutBtn");
-  let faqsBtn = document.querySelector("#faqsBtn");
+// function switchFront (id) {
+//   let sectionArr = document.querySelectorAll(".section");
+//   let aboutBtn = document.querySelector("#aboutBtn");
+//   let faqsBtn = document.querySelector("#faqsBtn");
   
-  sectionArr.forEach(function(elem, index){
-    elem.classList.add("display--hidden")
-  });
+//   sectionArr.forEach(function(elem, index){
+//     elem.classList.add("display--hidden")
+//   });
   
-  if (id === "about"){
-    if (aboutBtn.textContent === "about"){
-      document.querySelector("#about").classList.remove("display--hidden");
-      aboutBtn.textContent = "home";
-    } else {
-      document.querySelector("#home").classList.remove("display--hidden");
-      aboutBtn.textContent = "about";
-    }
-  } 
+//   if (id === "about"){
+//     if (aboutBtn.textContent === "about"){
+//       document.querySelector("#about").classList.remove("display--hidden");
+//       aboutBtn.textContent = "home";
+//     } else {
+//       document.querySelector("#home").classList.remove("display--hidden");
+//       aboutBtn.textContent = "about";
+//     }
+//   } 
 
-  if (id === "faqs"){
-    if(faqsBtn.textContent === "FAQs"){
-      document.querySelector("#faqs").classList.remove("display--hidden");
-      faqsBtn.textContent = "links";
-    } else {
-      document.querySelector("#links").classList.remove("display--hidden");
-      faqsBtn.textContent = "FAQs";
-    }
-  }
-  }
+//   if (id === "faqs"){
+//     if(faqsBtn.textContent === "FAQs"){
+//       document.querySelector("#faqs").classList.remove("display--hidden");
+//       faqsBtn.textContent = "links";
+//     } else {
+//       document.querySelector("#links").classList.remove("display--hidden");
+//       faqsBtn.textContent = "FAQs";
+//     }
+//   }
+//   }
 
-document.querySelector("#aboutBtn").addEventListener("click", () => switchFront("about"));
-document.querySelector("#faqsBtn").addEventListener("click", () => switchFront("faqs"));
+// document.querySelector("#aboutBtn").addEventListener("click", () => switchFront("about"));
+// document.querySelector("#faqsBtn").addEventListener("click", () => switchFront("faqs"));
